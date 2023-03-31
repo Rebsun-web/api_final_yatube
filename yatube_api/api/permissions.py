e72161a376a9e65b1a5029e4aff36fef8a8ac45c
Nikita Voronkin <voronkinikita@gmail.com>
2023-03-31 21:25:05 +0200

    First draft

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,14 +4,14 @@ from rest_framework import permissions
 class OwnerOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (request.user.is_authenticated or
-                request.method in permissions.SAFE_METHODS)
+        return (request.user.is_authenticated
+                or request.method in permissions.SAFE_METHODS)
 
     def has_object_permission(self, request, view, obj):
         if view.action == 'retrieve':
             return request.method in permissions.SAFE_METHODS
-        return (request.user.is_authenticated or
-                request.method in permissions.SAFE_METHODS)
+        return (request.user.is_authenticated
+                or request.method in permissions.SAFE_METHODS)
 
 
 class AuthanticatedOnly(permissions.BasePermission):
